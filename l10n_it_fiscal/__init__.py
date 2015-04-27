664240aa8b0ef7a12cb397591b7037b8226a4b08
antoniov <info@shs-av.com>
2015-04-27 16:59:54 +0200

    Correct __init__.py

--- l10n_it_fiscal/__init__.py
+++ l10n_it_fiscal/__init__.py
@@ -18,5 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
+#
+# Italian Chart of Account
+#
 from openerp import SUPERUSER_ID
